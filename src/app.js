2141a9319c618cc2877fefda0e1a49f859a774bf
Meng Zhang <meng.zhang@myob.com>
2020-10-14 22:57:09 +1100

    Configured react route

--- src/app.js
+++ src/app.js
@@ -1,7 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import IndecisionApp from "./components/IndecisionApp";
+import { BrowserRouter, Route } from "react-router-dom";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
-ReactDOM.render(<p>This is my template</p>, document.getElementById("app"));
+const ExpenseDashboardPage = () => (
+  <div>This is from my dashboard component</div>
+);
+
+const AddExpensePage = () => (
+    <div>This is from my add expense component</div>
+  );
+
+const EditExpensePage = () => (
+    <div>This is from my edit expense component</div>
+  );
+const HelpPage = () => (
+    <div>This is from my help page component</div>
+  );
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={ExpenseDashboardPage} exact={true}/>
+      <Route path="/create" component={AddExpensePage} exact={true}/>
+      <Route path="/edit" component={EditExpensePage} exact={true}/>
+      <Route path="/help" component={HelpPage} exact={true}/>
+    </div>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById("app"));
